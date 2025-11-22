154619ee00ee1d921b8df31e53b023c385f5acf7
Brian-Yobra <britzpuler@gmail.com>
2025-11-22 18:45:22 +0300

    added api Support

--- Books/models.py
+++ Books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Books(models.Model):
+    book_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("Date published")
+    total_pages = models.IntegerField("Page Numbers")
+    read_pages = models.IntegerField("No of pages read")
+    done_read = models.BooleanField("True or False")
--- Books/views.py
+++ Books/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Books
+from .serializer import BookSerializer
 
-# Create your views here.
+class BooksViewSet(viewsets.ModelViewSet):
+    """
+    Full CRUD endpoint for Books.
+    Supports list, retrieve, create, update, delete.
+    """
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
